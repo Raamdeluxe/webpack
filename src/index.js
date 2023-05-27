88b2161a75d9bc6fea48f649363f55d4ab53eac8
Raamdeluxe <robin@raamdeluxe.nl>
2023-05-27 11:08:06 +0200

    update 2.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import generateJoke from "./generateJoke";
-import "./styles/main.scss";
-import laughing from "./assets/laughing.svg";
+import "./styles/form.scss";
 
-const laughImg = document.getElementById("laughImg");
-laughImg.src = laughing;
+// create a form element
+const form = document.createElement("form");
 
-const jokeBtn = document.getElementById("jokeBtn");
-jokeBtn.addEventListener("click", generateJoke);
+// create a input element for name
+const nameInput = document.createElement("input");
+nameInput.setAttribute("type", "text");
+nameInput.setAttribute("name", "name");
+nameInput.setAttribute("placeholder", "Enter your name");
 
-console.log(generateJoke());
+// create a input element for email
+const emailInput = document.createElement("input");
+emailInput.setAttribute("type", "email");
+emailInput.setAttribute("name", "email");
+emailInput.setAttribute("placeholder", "Enter your email");
+
+// create a submit button
+const submitBtn = document.createElement("button");
+submitBtn.setAttribute("type", "submit");
+submitBtn.innerText = "Submit";
+
+// append all the elements to the form
+form.appendChild(nameInput);
+form.appendChild(emailInput);
+form.appendChild(submitBtn);
+
+// append the form to the body
+document.body.appendChild(form);
